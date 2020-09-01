1c6913afec902771f186274e04104f696f3d7123
Henrik Finsberg <henriknf@simula.no>
2020-09-01 13:57:23 +0200

    Implement right addition and dot product

--- vector.py
+++ vector.py
@@ -10,10 +10,18 @@ class Vector3D:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z})"
 
+    def __radd__(self, other):
+        return self.__add__(other)
+
     def __add__(self, other):
         """Add two vectors
         """
-        return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+        if isinstance(other, (int, float)):
+            return Vector3D(self.x + other, self.y + other, self.z + other)
+        elif isinstance(other, Vector3D):
+            return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+        else:
+            raise TypeError(f"Cannot add Vector3D to object of type {type(other)}")
 
     def __eq__(self, other):
         """Return True if self == other,
@@ -21,10 +29,14 @@ class Vector3D:
         """
         return self.x == other.x and self.y == other.y and self.z == other.z
 
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
 
 if __name__ == "__main__":
     v = Vector3D(1, 4, 2)
+    v.dot(1)
 
-    u = Vector3D(1, 1, 1)
-    w = v + u
-    w == Vector3D(2, 5, 3)
+    # u = Vector3D(1, 1, 1)
+    # w = v + u
+    # w == Vector3D(2, 5, 3)
